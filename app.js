dba6cc76e52e1ceff4c389892d90af931379a377
hyoudouzixi <wang.13099@buckeyemail.osu.edu>
2023-03-10 09:10:58 -0500

    update on moving circle

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 let x = 0;
 let y = 0;
-let fr = 30; //starting FPS
 let clr;
-let xoff = 0.0;
-let xincrement = 0.01;
+let circleX = 100;
+let circleY = 0;
+let xSpeed = 1;
+let ySpeed = 1;
+let circleSize = 20;
+let circleSpeedX = 5;
+let circleSpeedY = 5;
 
 
 function setup() {
   myCanvas = createCanvas(700, 480);
-  frameRate(fr); // Attempt to refresh at starting FPS
   clr = color(0, 0, 0);
   noStroke();
 }
 
 function draw() {
-  fill(0, 10);
-  rect(0, 0, width, height);
-
-  let n = noise(xoff) * width;
-
-  // With each cycle, increment xoff
-  xoff += xincrement;
-  x = x + 5;
-  y += 5;
-  if (x > width) {
-    x = 0;
-  }
-  if (y > 255) {
-    y = 0;
-  }
   background(150);
-  clr = color(0, y, y);
-
-  translate(0, 0);
+  push();
   fill(clr);
+  translate(0, 0);
   beginShape();
   vertex(x, 0);
   vertex(x + 10, 0); 
@@ -43,6 +30,48 @@ function draw() {
   vertex(x + 10, 20); 
   endShape(CLOSE);
 
+  translate(0, 0);
+  beginShape();
+  vertex(x + 20, 0);
+  vertex(x + 30, 0); 
+  vertex(x + 40, 20); 
+  vertex(x + 30, 40); 
+  vertex(x + 20, 40); 
+  vertex(x + 30, 20); 
+  endShape(CLOSE);
+
+  x = x + 5;
+  y += 5;
+  if (x > width) {
+    x = 0;
+  }
+  if (y > 255) {
+    y = 0;
+  }
+  
+  clr = color(0, y, y);
+  pop();
+
+  push();
+  circle(circleX, circleY, 50);
+  fill(255, 255, 0);
+
+  // modify state
+  circleX = circleX + xSpeed;
+  circleY = circleY + ySpeed;
+
+  //bounce off left and right
+  if(circleX < 0 || circleX > width) {
+    xSpeed = xSpeed * -1;
+    fill(255, 0, 0);
+  }
+
+  // bounce off top and bottom
+  if(circleY < 0 || circleY > height) {
+    ySpeed = ySpeed * -1;
+    fill(0, 0, 255);
+  }
+  pop();
 
 }
 
